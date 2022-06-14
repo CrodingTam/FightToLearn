0469fc77b2b74a8cb1b25cdd65d6145217bf6c65
SzTamass <91873880+SzTamass@users.noreply.github.com>
2022-06-14 09:29:39 +0300

    small updates

--- src/engine/GameManager.ts
+++ src/engine/GameManager.ts
@@ -1,12 +1,5 @@
 export class GameManager{
     constructor(canvas: HTMLCanvasElement){    
-        this.jancsi();
     }
 
-    public jancsi(){
-        const a = 10;
-        const b = 20;
-        let c = a + b;
-        console.log(c)
-  }
 }
